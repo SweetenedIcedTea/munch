77a2b4ff8a2703792e3a2b65c6d219c756067134
oldpepper12 <oldpepper12@gmail.com>
2019-10-26 14:38:13 -0400

    bruh

---
+++ munch_app/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Lewd wig is gay</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
