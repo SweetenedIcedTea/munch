e062d1c6af8d706ffe67b115d4a20f7745d20266
Ian Huang <ianhuang46@gmail.com>
2019-10-26 22:51:07 -0400

    Added data parsing

--- munch_mobile/src/API.tsx
+++ munch_mobile/src/API.tsx
@@ -2,22 +2,42 @@ export default class MunchServer {
     address: string;
     constructor(address: string) {
         this.address = address;
-
     }
-    async post(endpoint:string, data: any) {
+    async testConnection(): Promise<boolean> {
+        return (await this.post("echo", "bruh") == "bruh");
+    }
+    async getMenu(id: string | number) {
+        return this.get("menu/"+id);
+    }
+    async get(endpoint: string) {
         try {
-            const response = await fetch(this.address+'/'+endpoint, {
-                method: 'POST',
+            const response = await fetch(this.address + "/" + endpoint, {
+                method: "GET",
                 headers: {
-                    Accept: 'application/json',
-                    'Content-Type': 'application/json',
+                    Accept: "application/json",
+                    "Content-Type": "application/json"
                 },
-                body: JSON.stringify(data),
             });
             const responseData = response.json();
             return responseData;
         } catch (error) {
-            console.log("[API Error] "+error); // bruh
+            console.log("[API Error] " + error); // bruh
+        }
+    }
+    async post(endpoint: string, data: any) {
+        try {
+            const response = await fetch(this.address + "/" + endpoint, {
+                method: "POST",
+                headers: {
+                    Accept: "application/json",
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(data)
+            });
+            const responseData = response.json();
+            return responseData;
+        } catch (error) {
+            console.log("[API Error] " + error); // bruh
         }
     }
 }
